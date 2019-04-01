195ca8455a392f63d31adfd596b951ef7ade78b0
Joel S <joel29dec@gmail.com>
2019-04-01 21:48:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,3 +9,12 @@ end
 
 
 print reverse_each_word("Hello there, and how are you?")
+
+#def reverse_each_word(phrase)
+  #arr = phrase.split(" ")
+  #sentence = []
+  #arr.each do |x|
+  #sentence << x.reverse
+  #end
+  #sentence.join(" ")
+#end
